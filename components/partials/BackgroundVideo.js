39e1c04157b062316b75760d2b73ca0dd9637c41
Liannus <josh-gorman@hotmail.com>
2018-11-13 13:18:44 -0500

    ran export, temp fix for video URL

--- components/partials/BackgroundVideo.js
+++ components/partials/BackgroundVideo.js
@@ -11,7 +11,7 @@ export default class backgroundVideo extends React.Component {
     return (
       <>
         <video id="my-video" fluid loop muted autoPlay preload="auto" poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
-            <source src="/static/Timelapse.mp4" type='video/mp4'></source>
+            <source src="/Liannus.github.io2/static/Timelapse.mp4" type='video/mp4'></source>
             <p className="vjs-no-js">
               To view this video please enable JavaScript, and consider upgrading to a web browser that
               <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
