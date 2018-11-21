f68a118d19b6e5bb78acae49a4e82da6904243ac
Liannus <josh-gorman@hotmail.com>
2018-11-21 11:21:49 -0500

    Split card components for ease of editing, added reactstrap to cards

--- components/Projects.js
+++ components/Projects.js
@@ -1,92 +1,21 @@
 import React from 'react';
-import { Card, Button, CardImg, CardTitle, CardText, CardColumns,
- CardSubtitle, CardBody } from 'reactstrap';
+import ProjectCard from './partials/ProjectCard.js'
 
 const Projects = (props) => {
   return (
     <>
-    <div class="row">
-      <div class="col-md-6">
-        <div class="card flex-md-row mb-4 shadow-sm h-md-250">
-          <div class="card-body d-flex flex-column align-items-start">
-            <strong class="d-inline-block mb-2 text-primary">World</strong>
-            <h6 class="mb-0">
-              <a class="text-dark" href="#">40 Percent of People Can’t Afford Basics</a>
-            </h6>
-            <div class="mb-1 text-muted small">Nov 12</div>
-            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a class="btn btn-outline-primary btn-sm" role="button" href="http://www.jquery2dotnet.com/">Continue reading</a>          </div>
-          <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="//placeimg.com/250/250/arch" style={{width: "200px", height: "250px"}} />
+      <h2> PROJECTS </h2>
+      <div className="row">
+        <div className="col-md-6">
+          <ProjectCard imgSide="left"/>
+        </div>
+        <div className="col-md-6">
+          <ProjectCard imgSide="right"/>
+        </div>
+        <div className="col-md-6">
+          <ProjectCard imgSide="left"/>
         </div>
       </div>
-      <div class="col-md-6">
-        <div class="card flex-md-row mb-4 shadow-sm h-md-250">
-          <div class="card-body d-flex flex-column align-items-start">
-            <strong class="d-inline-block mb-2 text-primary">World</strong>
-            <h6 class="mb-0">
-              <a class="text-dark" href="#">40 Percent of People Can’t Afford Basics</a>
-            </h6>
-            <div class="mb-1 text-muted small">Nov 12</div>
-            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a class="btn btn-outline-primary btn-sm" role="button" href="http://www.jquery2dotnet.com/">Continue reading</a>          </div>
-          <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="//placeimg.com/250/250/arch" style={{width: "200px", height: "250px"}} />
-        </div>
-      </div>
-      <div class="col-md-6">
-        <div class="card flex-md-row mb-4 shadow-sm h-md-250">
-          <div class="card-body d-flex flex-column align-items-start">
-            <strong class="d-inline-block mb-2 text-primary">World</strong>
-            <h6 class="mb-0">
-              <a class="text-dark" href="#">40 Percent of People Can’t Afford Basics</a>
-            </h6>
-            <div class="mb-1 text-muted small">Nov 12</div>
-            <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-            <a class="btn btn-outline-primary btn-sm" role="button" href="http://www.jquery2dotnet.com/">Continue reading</a>          </div>
-          <img class="card-img-right flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="//placeimg.com/250/250/arch" style={{width: "200px", height: "250px"}} />
-        </div>
-      </div>
-      </div>
-    <CardColumns>
-        <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-      </Card>
-      <Card>
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This card has supporting text below as a natural lead-in to additional content.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card body inverse style={{ backgroundColor: '#333', borderColor: '#333' }}>
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button>Button</Button>
-      </Card>
-      <Card>
-        <CardImg top width="100%" src="https://placeholdit.imgix.net/~text?txtsize=33&txt=256%C3%97180&w=256&h=180" alt="Card image cap" />
-        <CardBody>
-          <CardTitle>Card title</CardTitle>
-          <CardSubtitle>Card subtitle</CardSubtitle>
-          <CardText>This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</CardText>
-          <Button>Button</Button>
-        </CardBody>
-      </Card>
-      <Card body inverse color="primary">
-        <CardTitle>Special Title Treatment</CardTitle>
-        <CardText>With supporting text below as a natural lead-in to additional content.</CardText>
-        <Button color="secondary">Button</Button>
-      </Card>
-    </CardColumns>
     </>
   );
 };
--- components/partials/ProjectCard.js
+++ components/partials/ProjectCard.js
@@ -1,15 +1,22 @@
-import react from 'react';
+import React from 'react';
+import { Card, Button, CardImg, CardTitle, CardText, CardColumns,
+ CardSubtitle, CardBody } from 'reactstrap';
 
-export default class ProjectCard extends react.component {
-  constructor(props) {
-    super(props);
-  }
+const ProjectCard = (props) => {
+  return (
+    <>
+      <Card className="flex-md-row mb-4 shadow-sm h-md-250">
+        {props.imgSide=="left" && <CardImg className="flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="//placeimg.com/250/250/arch" style={{width: "200px", height: "250px"}} /> }
+        <CardBody className="d-flex flex-column align-items-start">
+          <CardTitle className="d-inline-block mb-2 text-primary">{props.imgSide}</CardTitle>
+          <CardSubtitle className="mb-1 text-muted small">Card subtitle</CardSubtitle>
+          <CardText className="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</CardText>
+          <Button className="btn-outline-primary btn-sm">Button</Button>
+        </CardBody>
+        {props.imgSide=="right" && <CardImg className="flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="//placeimg.com/250/250/arch" style={{width: "200px", height: "250px"}} /> }
+      </Card>
+    </>
+  );
+};
 
-  render() {
-    return (
-      <>
-        <p>some information inside of the about page</p>
-      </>
-    );
-  }
-}
+export default ProjectCard;
