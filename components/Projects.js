07a2d5605c6dbcbe40b70002db54185b7bf9ae4e
Liannus <josh-gorman@hotmail.com>
2018-12-07 08:46:45 -0500

    Added Lists and iteration for component generation.

--- components/Projects.js
+++ components/Projects.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import ProjectCard from './partials/ProjectCard.js'
 
+const cards = [
+      {
+        projectTitle: "Forest Fire",
+        subTitle: "React, NPM and Webpack",
+        content: 1,
+        link: 1,
+        imgSide: "left"
+      },
+      {
+        projectTitle: "Recipe Repo",
+        subTitle: "NodeJS, MongoDB, Express and Pug",
+        content: 1,
+        link: 1,
+        imgSide: "right"
+      },
+      {
+        projectTitle: "Warframe Tracker",
+        subTitle: "Electron, Redux and Jest",
+        content: 1,
+        link: 1,
+        imgSide: "left"
+      },
+      {
+        projectTitle: "Path of Tools",
+        subTitle: "?",
+        content: 1,
+        link: 1,
+        imgSide: "right"
+      }
+  ];
 const Projects = (props) => {
   return (
     <div>
       <h2>Projects</h2>
       <div className="row">
-        <div className="col-md-6">
-          <ProjectCard imgSide="left"/>
+      {cards.map(i=> {
+        return(
+        <div key={i.projectTitle} className="col-md-6">
+          <ProjectCard key={i.projectTitle}
+                       projectTitle={i.projectTitle}
+                       subTitle={i.subTitle}
+                       content={i.content}
+                       link={i.link}
+                       imgSide={i.imgSide}/>
         </div>
-        <div className="col-md-6">
-          <ProjectCard imgSide="right"/>
-        </div>
-        <div className="col-md-6">
-          <ProjectCard imgSide="left"/>
-        </div>
-        <div className="col-md-6">
-          <ProjectCard imgSide="right"/>
-        </div>
+        );
+      })}
       </div>
       <style jsx> {`
         h2 {
--- components/partials/ProjectCard.js
+++ components/partials/ProjectCard.js
@@ -8,10 +8,10 @@ const ProjectCard = (props) => {
       <div className="card flex-md-row mb-4 shadow-sm h-md-250">
         {props.imgSide=="left" && <img className="card-img flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="//placeimg.com/250/250/arch" style={{width: "200px", height: "250px"}} /> }
         <div className="card-body d-flex flex-column align-items-start">
-          <h3 className="card-title d-inline-block mb-2 text-primary">{props.imgSide}</h3>
-          <h4 className="card-subtitle mb-1 text-muted small">Card subtitle</h4>
-          <p className="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-          <Button className="btn-outline-primary btn-sm">Button</Button>
+          <h3 className="card-title d-inline-block mb-2 text-primary">{props.projectTitle}</h3>
+          <h4 className="card-subtitle mb-1 text-muted small">{props.subTitle}</h4>
+          <p className="card-text mb-auto">{props.content}</p>
+          <Button className="btn-outline-primary btn-sm">Project Link</Button>
         </div>
         {props.imgSide=="right" && <img className="card-img flex-auto d-none d-lg-block" alt="Thumbnail [200x250]" src="//placeimg.com/250/250/arch" style={{width: "200px", height: "250px"}} /> }
       </div>
