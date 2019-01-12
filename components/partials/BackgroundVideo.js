e3354c01037d53087a4dbb7359cdb88b08bcd2a2
Iphiram <josh-gorman@hotmail.com>
2019-01-12 16:48:49 -0500

    changed vide

--- components/partials/BackgroundVideo.js
+++ components/partials/BackgroundVideo.js
@@ -9,9 +9,8 @@ export default class backgroundVideo extends React.Component {
   render() {
     return (
       <>
-        <video id="my-video" loop muted autoPlay preload="auto" poster="MY_VIDEO_POSTER.jpg" data-setup="{}">
+        <video id="my-video" loop muted autoPlay preload="auto" poster="static/background.PNG" data-setup="{}">
             <source src="static/Timelapse.mp4" type='video/mp4'></source>
-          /*<source src="/Liannus.me/static/Timelapse.mp4" type='video/mp4'></source>*/
             <p className="vjs-no-js">
               To view this video please enable JavaScript, and consider upgrading to a web browser that
               <a href="https://videojs.com/html5-video-support/" target="_blank">supports HTML5 video</a>
