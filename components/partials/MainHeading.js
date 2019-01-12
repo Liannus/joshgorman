830ad3fc5ee78bcdfff91fb518c7cb787bb9ca8a
Iphiram <josh-gorman@hotmail.com>
2019-01-12 18:04:55 -0500

    fix text colour

--- components/partials/MainHeading.js
+++ components/partials/MainHeading.js
@@ -29,7 +29,7 @@ export default class MainHeading extends React.Component {
             }
           }
           h1 {
-            color: red;
+            color: white;
             text-shadow: 2px 2px 3px #444;
             font-size: 3em;
             font-weight: 700;
